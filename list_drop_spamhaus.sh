97678dab2d59484d10fa800a4112a8a71706988a
Figueroa Pablo <89981340+pablomdz@users.noreply.github.com>
2022-09-29 21:04:32 -0300

    Update list_drop_spamhaus.sh

--- list_drop_spamhaus.sh
+++ list_drop_spamhaus.sh
@@ -1,10 +1,10 @@
-!/bin/bash
+#!/bin/bash
 DROP_LIST=$(curl -s http://www.spamhaus.org/drop/drop.txt | grep -v "^;" | awk {'print $1'})
 ipset create spamhaus hash:net -exist
 ipset create spamhaus_new hash:net -exist
 ipset flush spamhaus_new
 for net in $DROP_LIST
-do
+  do
 ipset add spamhaus_new $net
 done
 ipset swap spamhaus spamhaus_new
